dfa501d26b833035eee335041322dd358231c7f0
tarunakash66-lgtm <tarunakash66@gmail.com>
2026-06-11 22:21:33 +0530

    Update main.c
    Added canvas initialization

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+#define ROWS 10
+#define COLS 20
+
+char canvas[ROWS][COLS];
+
+void initializeCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            printf("%c ", canvas[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf("Mini Project Started\n");
+    initializeCanvas();
+
+    displayCanvas();
+
     return 0;
 }
